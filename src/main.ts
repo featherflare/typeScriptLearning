d197d92cae6922f711a55feff6d776c6f2a4872b
max43607 <max43607@gmail.com>
2023-05-16 13:36:58 +0700

    Lession05-09 && Set up vite typescript vanilla project

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
-let myName: string = 'Max'
-let meaningOfLive: number
-let isLoading: boolean
-let album: string | number
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
 
-myName = 'John'
-meaningOfLive = 42
-isLoading = true
-album = 5150
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
 
-const sum = (a: string, b: string) => {
-  return a + b
-}
-
-let postId: string | number
-let isActive: number | boolean
-
-let re: RegExp = /\w+/g
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
